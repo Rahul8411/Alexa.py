15e3b7023c4f18d3cfaf867d770e6eff6f3e1b6b
Rahul8411 <93394736+Rahul8411@users.noreply.github.com>
2021-10-29 22:30:24 +0530

    Add files via upload

---
+++ Alexa.py
@@ -0,0 +1,79 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import pyttsx3 
+import webbrowser
+import os
+import smtplib
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        talk("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        talk("Good Afternoon!")   
+
+    else:
+        talk("Good Evening!")  
+
+    talk("I am Alexa.  GrandMaa Please tell me how may I help you")
+if __name__ == "__main__":
+    wishMe()       
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'wikipedia' in command:
+        person = command.replace('wikipedia', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'open google' in command:
+            webbrowser.open("google.com")  
+    else:
+        talk('Please say the command again.')
+
+while True:
+    run_alexa()
+
+
+
+
+
+
